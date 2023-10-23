955087a7c0625b066a112ddee678fadbbec50559
Caleb <xxgoldenprozxx@gmail.com>
2023-10-23 22:20:15 +0800

    Removed basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/ChatVibe'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
